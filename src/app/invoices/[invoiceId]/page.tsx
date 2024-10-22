6ace024d183201b0e43682257f9e9f9e80f608fe
Zsombinszky <zsombi.studio@gmail.com>
2024-10-22 10:49:45 +0200

    invoiceId page types refactored

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,9 +1,9 @@
 import {db} from '@/db';
-import {Customers, Invoices} from '@/db/schema'
+import {Customers, Invoices} from '@/db/schema';
 import {and, eq, isNull} from "drizzle-orm";
 
 import {notFound} from "next/navigation";
-import {auth} from '@clerk/nextjs/server'
+import {auth} from '@clerk/nextjs/server';
 
 import Invoice from "@/app/invoices/[invoiceId]/invoice";
 
@@ -15,30 +15,32 @@ export default async function InvoicePage({params}: { params: { invoiceId: strin
 
     const invoiceId = parseInt(params.invoiceId);
     if (isNaN(invoiceId)) {
-        throw new Error("Invalid Invoice ID")
+        throw new Error("Invalid Invoice ID");
     }
 
     let result;
     if (orgId) {
-        [result] = await db.select().from(Invoices)
+        [result] = await db.select()
+            .from(Invoices)
             .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
             .where(and(eq(Invoices.id, invoiceId), eq(Invoices.organizationId, orgId)))
             .limit(1);
     } else {
-        [result] = await db.select().from(Invoices)
+        [result] = await db.select()
+            .from(Invoices)
             .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
             .where(and(eq(Invoices.id, invoiceId), eq(Invoices.userId, userId), isNull(Invoices.organizationId)))
             .limit(1);
     }
 
     if (!result) {
-        notFound()
+        notFound();
     }
 
     const invoice = {
         ...result.invoices,
-        customer: result.customers
-    }
+        customer: result.customers,
+    };
 
-    return <Invoice invoice={invoice}/>
+    return <Invoice invoice={invoice}/>;
 }
